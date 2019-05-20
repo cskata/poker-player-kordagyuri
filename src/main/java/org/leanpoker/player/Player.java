aabdaa35a7cef1b50d60b7668db6a89a53149391
Csaba Szabó <szabcsabest@gmail.com>
2019-05-20 13:41:02 +0200

    refactor

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,54 +9,33 @@ import org.leanpoker.player.card.PokerCard;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Player {
 
-    static final String VERSION = "Fallout 2.7";
+    static final String VERSION = "Fallout 2.8";
+    private static List<PokerCard> communityCards = new ArrayList<>();
+    private static Gson gson = new GsonBuilder().create();
+    private static JsonObject gyuri;
+    private static List<PokerCard> holeCards = new ArrayList<>();
 
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
+        getGyuri(jsonObject);
+        getHoleCards();
+        getCommunityCards(jsonObject);
+
+        return evaluateCards(jsonObject);
+    }
+
+    private static int evaluateCards(JsonObject jsonObject) {
         int buyIn = jsonObject.get("current_buy_in").getAsInt();
 
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        JsonArray communityCards = jsonObject.getAsJsonArray("community_cards");
-        List<PokerCard> community = new ArrayList<>();
-
-        for(int i=0; i<communityCards.size(); i++){
-            community.add(gson.fromJson(communityCards.get(i), PokerCard.class));
-        }
-
-        System.err.println(community);
-
-
-        JsonArray players = jsonObject.get("players").getAsJsonArray();
-
-        List<JsonObject> playersAsString = IntStream.range(0, players.size())
-                .mapToObj(index -> (players.get(index)).getAsJsonObject())
-                .filter(object -> object.get("name").getAsString().equals("KordaGyuri"))
-                .collect(Collectors.toList());
-
-        JsonObject currentPlayer = playersAsString.get(0);
-
-        JsonArray playerCards = currentPlayer.getAsJsonArray("hole_cards");
-        List<PokerCard> holeCards = new ArrayList<>();
-
-        for(int i=0; i<playerCards.size(); i++){
-            holeCards.add(gson.fromJson(playerCards.get(i), PokerCard.class));
-        }
-
-        System.err.println(currentPlayer.get("name").getAsString());
-
-        System.err.println(holeCards);
-
         PokerCard hand1 = holeCards.get(0);
-        PokerCard hand2 =holeCards.get(1);
+        PokerCard hand2 = holeCards.get(1);
 
-        if(buyIn > 800) {
+        if (buyIn > 800) {
             if (hand1.getRank().equals(hand2.getRank()) || hand1.getRank().matches("[JQKA]") && hand2.getRank().matches("[JQKA]")) {
                 return buyIn + 10;
             } else {
@@ -65,6 +44,34 @@ public class Player {
         } else {
             return buyIn + 1;
         }
+    }
+
+    private static void getHoleCards() {
+        holeCards.clear();
+        JsonArray playerCards = gyuri.getAsJsonArray("hole_cards");
+        for (int i = 0; i < playerCards.size(); i++) {
+            holeCards.add(gson.fromJson(playerCards.get(i), PokerCard.class));
+        }
+        System.err.println(holeCards);
+    }
+
+    private static void getGyuri(JsonObject jsonObject) {
+        JsonArray players = jsonObject.get("players").getAsJsonArray();
+        List<JsonObject> playersAsString = IntStream.range(0, players.size())
+                .mapToObj(index -> (players.get(index)).getAsJsonObject())
+                .filter(object -> object.get("name").getAsString().equals("KordaGyuri"))
+                .collect(Collectors.toList());
+        gyuri = playersAsString.get(0);
+        System.err.println(gyuri.get("name").getAsString());
+    }
+
+    private static void getCommunityCards(JsonObject jsonObject) {
+        communityCards.clear();
+        JsonArray communityCards = jsonObject.getAsJsonArray("community_cards");
+        for (int i = 0; i < communityCards.size(); i++) {
+            Player.communityCards.add(gson.fromJson(communityCards.get(i), PokerCard.class));
+        }
+        System.err.println(Player.communityCards);
     }
 
     public static void showdown(JsonElement game) {
