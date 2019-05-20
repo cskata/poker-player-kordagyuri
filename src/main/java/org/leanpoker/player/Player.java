754b2960c4c2f9d1e7f81fb81d0a5173b0f4eb88
Csaba Szabó <szabcsabest@gmail.com>
2019-05-20 10:20:56 +0200

    read buy in from json

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,18 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Ghost Protocol";
+    static final String VERSION = "Ghost Protocol 2.0";
 
     public static int betRequest(JsonElement request) {
-        return 2;
+        JsonObject jsonObject = request.getAsJsonObject();
+        int buyIn = jsonObject.get("current_buy_in").getAsInt();
+        return buyIn + 1;
     }
 
     public static void showdown(JsonElement game) {
