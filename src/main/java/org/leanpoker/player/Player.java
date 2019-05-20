e77da132b44c62cac9dcd4320cf440deb7e0d394
Katalin Csortos <katalin.csortos@gmail.com>
2019-05-20 10:53:49 +0200

    can we get Gyuri ba?

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,9 +1,13 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Player {
 
@@ -12,6 +16,15 @@ public class Player {
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
         int buyIn = jsonObject.get("current_buy_in").getAsInt();
+        JsonArray players = jsonObject.get("players").getAsJsonArray();
+
+        List<JsonObject> playersAsString = IntStream.range(0, players.size())
+                .mapToObj(index -> (players.get(index)).getAsJsonObject())
+                .filter(object -> object.get("name").getAsString().equals("KordaGyuri"))
+                .collect(Collectors.toList());
+
+        JsonObject currentPlayer = playersAsString.get(0);
+
         return buyIn + 1;
     }
 
