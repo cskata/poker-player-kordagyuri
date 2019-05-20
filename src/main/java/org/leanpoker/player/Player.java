4e00bc70e9b69b3787989acd06072866b7ed9739
gaborBKI <gabor.bki@gmail.com>
2019-05-20 10:56:07 +0200

    Tried to get community cards and turn them into objects.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,8 +1,14 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.leanpoker.player.card.PokerCard;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
@@ -12,6 +18,16 @@ public class Player {
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
         int buyIn = jsonObject.get("current_buy_in").getAsInt();
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        JsonArray communityCards = jsonObject.getAsJsonArray("community_cards");
+        List<PokerCard> community = new ArrayList<>();
+
+        for(int i=0; i<communityCards.size(); i++){
+            community.add(gson.fromJson(communityCards.get(i), PokerCard.class));
+        }
+
         return buyIn + 1;
     }
 
---
+++ src/main/java/org/leanpoker/player/card/PokerCard.java
@@ -0,0 +1,8 @@
+package org.leanpoker.player.card;
+
+public class PokerCard {
+
+    private int rank;
+    private String suit;
+
+}
