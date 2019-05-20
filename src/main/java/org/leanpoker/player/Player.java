ddf04c7131a82f249741fff6ddc5a66793668a99
gaborBKI <gabor.bki@gmail.com>
2019-05-20 12:12:09 +0200

    Tried to get community cards and turn them into objects.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -15,7 +15,7 @@ import java.util.stream.IntStream;
 
 public class Player {
 
-    static final String VERSION = "Fallout 2.1";
+    static final String VERSION = "Fallout 2.7";
 
     public static int betRequest(JsonElement request) {
         JsonObject jsonObject = request.getAsJsonObject();
@@ -57,7 +57,7 @@ public class Player {
         PokerCard hand2 =holeCards.get(1);
 
         if(buyIn > 800) {
-            if (hand1.getRank().equals(hand2.getRank())) {
+            if (hand1.getRank().equals(hand2.getRank()) || hand1.getRank().matches("[JQKA]") && hand2.getRank().matches("[JQKA]")) {
                 return buyIn + 10;
             } else {
                 return 0;
